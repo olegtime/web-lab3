de40fcb76a85ef0a1c59ffe8ec0f61bc563eb732
niik <fk724s@mail.ru>
2024-01-06 20:11:47 +0300

    fixed naming

--- src/main/webapp/script/drawer.js
+++ src/main/webapp/script/drawer.js
@@ -3,10 +3,10 @@ import {checkStatus} from "./utils";
 export function drawPreview(x, y, r) {
     const circle = document.getElementById('point')
 
-    const r_label = document.getElementsByClassName("r-label");
-    const r_minus_label = document.getElementsByClassName("r-minus-label");
-    const r_half_label = document.getElementsByClassName("r-half-label");
-    const r_minus_half_label = document.getElementsByClassName("r-half-minus-label");
+    const rLabel = document.getElementsByClassName("r-label");
+    const rMinusLabel = document.getElementsByClassName("r-minus-label");
+    const rHalfLabel = document.getElementsByClassName("r-half-label");
+    const rMinusHalfLabel = document.getElementsByClassName("r-half-minus-label");
 
     const centerX = 200;
     const centerY = 200;
@@ -19,20 +19,21 @@ export function drawPreview(x, y, r) {
     circle.setAttribute("cx", scaledX.toString());
     circle.setAttribute("cy", scaledY.toString());
 
+
     // set values instead of R markers
-    for (let label of r_label) {
+    for (let label of rLabel) {
         label.innerHTML = r.toString();
     }
 
-    for (let label of r_minus_label) {
+    for (let label of rMinusLabel) {
         label.innerHTML = (-r).toString();
     }
 
-    for (let label of r_half_label) {
+    for (let label of rHalfLabel) {
         label.innerHTML = (r/2).toString();
     }
 
-    for (let label of r_minus_half_label) {
+    for (let label of rMinusHalfLabel) {
         label.innerHTML = (-r/2).toString();
     }
 
@@ -66,9 +67,7 @@ export function safeNewDraw(xValues, y, r) {
     if (xValues.length === 0) {
         drawNewPoint(0, y, r)
     } else {
-        xValues.forEach(x => {
-            drawNewPoint(x, y, r)
-        })
+        xValues.forEach(x => drawNewPoint(x, y, r))
     }
 }
 
@@ -76,8 +75,6 @@ export function safePreDraw(xValues, y, r) {
     if (xValues.length === 0) {
         drawPreview(0, y, r)
     } else {
-        xValues.forEach(x => {
-            drawPreview(x, y, r)
-        })
+        xValues.forEach(x => drawPreview(x, y, r))
     }
 }
